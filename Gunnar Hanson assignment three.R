2f0e99f8d108cf4477a1733203d4864f7c04497d
Gunnar Hanson <gch224@lehigh.edu>
2022-10-07 01:51:28 -0400

    git commit

---
+++ Gunnar Hanson assignment three.R
@@ -0,0 +1,43 @@
+ibrary(tidyverse)
+library(here)
+getwd()
+US2020 <- read_csv(here("covid-19-data", "us-counties-2020.csv"))
+US2021 <- read_csv(here ("covid-19-data", "us-counties-2021.csv"))
+US2022 <- read_csv(here ("covid-19-data", "us-counties-2022.csv"))
+USRECENT <- read_csv(here ("covid-19-data", "us-counties-recent.csv"))
+USLIVE <- read_csv(here ("covid-19-data","live", "us-counties.csv"))
+COMBINE <- rbind(US2020, US2021, US2022, USRECENT, USLIVE)
+
+# filter
+
+PACounty <- filter(COMBINE, state == "Pennsylvania")
+Lehigh <- filter(PACounty, county=="Lehigh")
+
+#remove repeating rows
+
+LehighCases <- distinct(Lehigh, date, .keep_all = TRUE)
+
+# new case amount
+
+n <- length(LehighCases$date)
+LehighCases$increase_cases <- 0 
+
+# loop
+
+for (i in 2: n)
+{
+  LehighCases$increase_cases[i]<- (LehighCases$cases[i]-LehighCases$cases[i-1])
+}
+view(LehighCases)
+
+# graph data
+
+p <- ggplot(data = LehighCases)
+
+p + geom_line(color="blue", mapping =aes(x=date, 
+                                         y=increase_cases),
+              group=1)+ 
+  labs(x = "Date", y = "Increased COVID Cases", title = "COVID-19 Cases In Lehigh, PA")
+
+view(p)
+
